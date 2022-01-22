84ee5e9bb6bdae04c2fe55fc875a85afcce8164f
JD <jann.driessen@gmx.de>
2022-01-22 14:53:35 -0500

    Add main box elements

--- packages/next-app/src/components/MainBox.tsx
+++ packages/next-app/src/components/MainBox.tsx
@@ -1,15 +1,44 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 
+import { Button } from "components/Button";
+
+// TODO: add state isConnected (switch button when true)
 export const MainBox = () => {
+  const primaryButtonProps = {
+    w: "100%",
+  };
+  const learnMoreButtonProps = {};
   return (
-    <Box
-      w={["320px"]}
-      h="200px"
-      mh={["24px", "0"]}
-      backgroundColor="white"
-      borderRadius="24px"
-      position="absolute"
-      top="35vh"
-    />
+    <Box my={["24px", "0"]} w="100%">
+      <Heading
+        as="h1"
+        color="white"
+        fontSize={["44px", "96px"]}
+        fontWeight="500"
+      >
+        Airdrop
+      </Heading>
+      <Text
+        mt="5"
+        mb="8"
+        color="white"
+        fontSize={["20px", "24px"]}
+        fontWeight="500"
+      >
+        <span style={{ fontStyle: "italic" }}>$CODE</span> is the new governance
+        token for Developer DAO. Connect your wallet to determine your airdrop
+        eligibility.
+      </Text>
+      <Flex direction={["column", "row"]}>
+        <Box mb={["4", "0"]} mr={["0", "7"]} w={["100%", "inherit"]}>
+          <Button
+            primary={true}
+            label="CONNECT WALLET"
+            {...primaryButtonProps}
+          />
+        </Box>
+        <Button label="LEARN MORE" />
+      </Flex>
+    </Box>
   );
 };
